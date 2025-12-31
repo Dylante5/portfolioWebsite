9973688dda7a9f92aee94336b8f1d8d03f770841
Dylan Everett <dylan@olympiapharmacy.com>
2025-12-31 13:23:49 -0500

    added styling to games and templates routes, added placeholders

--- src/pages/Templates.tsx
+++ src/pages/Templates.tsx
@@ -8,8 +8,16 @@ export default function AppTemplates() {
     });
 
     return (
-        <div className="flex flex-col">
-            <h1 className="w-full text-center">Templates</h1>
+        <div className="mx-auto w-full max-w-6xl md:max-w-7xl space-y-10 my-12 md:my-6 px-6 md:px-0 ">
+            <div className="pointer-events-none absolute -inset-6 bg-[url('/pattern-light.svg')]
+                dark:bg-[url('/pattern-dark.svg')] opacity-[0.06] dark:opacity-[0.1] w-full" />
+            <section className="relative overflow-hidden rounded-2xl border border-[rgb(var(--border))] bg-[rgb(var(--card))]
+                p-6 shadow-md dark:shadow-[0_0_12px_rgba(255,255,255,0.2)] md:p-10">
+                <div className="flex flex-col gap-6 md:flex-row md:items-center md:justify-between">
+                    <h1 className="text-2xl font-bold text-center">Templates</h1>
+                    <p>TBD</p>
+                </div>
+            </section>
         </div>
     );
 }
